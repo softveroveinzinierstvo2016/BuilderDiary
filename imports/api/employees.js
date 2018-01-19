6e245436fecf5e09606d7736f2601dbe557b4092
Patrik <pato.sedlak6@gmail.com>
2018-01-19 15:46:49 +0100

    Pridavanie zamestnancov ---- v1.1 ----- co som premazal, vratil som spat

--- imports/api/employees.js
+++ imports/api/employees.js
@@ -6,9 +6,23 @@ export const Employees = new Mongo.Collection('employee');
 
 if(Meteor.isServer) {
     Meteor.publish('employees', function usersPublication() {
-        return Employees.find();
+        return Employees.find({},{fields: {login: 0, password: 0}});
     });
     Meteor.methods({
+        'employees.loggUser': function(loggName, password) {
+            if (!loggName || !password)
+                return null;
+            let user = Employees.findOne({login: loggName, password: password});
+            if (!user)
+                return null;
+            if (user.login != loggName || user.password != password)
+                return null;
+            user.id = user._id;
+            user.login = '';
+            user.password = '';
+            return user;
+        },
+
         'employee.insert': function(employee){
             console.log(employee);
             var id=Employees.insert({
