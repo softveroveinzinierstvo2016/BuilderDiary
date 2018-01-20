307ed9d957ed4392325658e6725c3bc28e3bcd0e
Slavomir Slovenkai <slavomir.slovenkai@student.upjs.sk>
2018-01-20 13:23:14 +0100

    fast preparation

--- imports/ui/EmployeeDetailView.jsx
+++ imports/ui/EmployeeDetailView.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 // Nav
-import Home from './Home';
 import App from './App';
+import EmployeesView from './EmployeesView';
 
 import { EmployeeService } from '../services/employeeService';
 
@@ -21,31 +21,27 @@ export default class EmployeeDetailView extends Component {
     }
 
     handleGoHome() {
-        render(<Home/>,document.getElementById('app'));
+        render(<EmployeesView/>,document.getElementById('app'));
     }
-
+    handleAddWageDeduction(){
+        // tu prepni na pridavanie zrazok
+    }
     bossView(){
-        // Todo: Dorobit polozku vyplatene , ostava vzplatit, zrazky
         return(
         <div>
-            <label>Meno: </label> <label>{this.employee.name}</label> <br/>
-            <label>Priezvisko: </label> <label>{this.employee.surname}</label> <br/>
-            <label>Login: </label> <label>{this.employee.login}</label> <br/>
-            <label>Rola: </label> <label>{this.employee.role}</label> <br/>
-            <label>Sum: </label> <label>{this.employee.sumAssistant}</label> <br/>
+            <button onClick={this.handleGoBack}></button> <br/>
+            <h1>{employeeService.getEmployeeName(this.employee.id)}</h1>
+            <button onClick={this.handleAddWageDeduction}>Pridať zrážky</button><br/>
         </div>
         );
     }
-
-
-
     render() {
         var view;
 
         view = this.bossView();
         return(
             <div className="container">
-                <button onClick={this.handleGoHome} >Domov</button> <br/>
+                <button onClick={this.handleGoHome} >Späť na zamestnancov</button> <br/>
                 {view}
             </div>
         );
